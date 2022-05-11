5a8847a600198f2541d4a7eaf4780a81b957473b
S L Albuquerque <devsulaalbuquerque@gmail.com>
2022-05-11 15:19:55 -0300

    stage 2

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -36,8 +37,10 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
 
     const [feedbackType, setfeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setfeedbackSent] = useState(false)
 
     function handleRestartFeedback() {
+        setfeedbackSent(false)
         setfeedbackType(null)
     }
     
@@ -45,13 +48,20 @@ export function WidgetForm() {
         <>
             <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
         
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setfeedbackType}/>
-                ) : (
-                 <FeedbackContentStep 
-                    feedbackType={feedbackType}
-                    onFeedbackRestartRequested={handleRestartFeedback}
-                />
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+            ) : (
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setfeedbackType}/>
+                        ) : (
+                        <FeedbackContentStep 
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setfeedbackSent(true)}
+                        />
+                    )}
+                </>
             )}
 
             <footer className="text-xs text-neutral-400">
