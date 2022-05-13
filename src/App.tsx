921f740bddec0ef836ebd899afce3c577f1f0196
S L Albuquerque <devsulaalbuquerque@gmail.com>
2022-05-12 21:20:12 -0300

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Widget } from "./components/Widget";
-import imageNLW from './assets/NLW-return- 2560x1600.svg'
 
 export function App() {
 
   return (
     <>
-      <img src={imageNLW} alt="Banner NLW Return"/>
       <Widget/>
     </>
   )
